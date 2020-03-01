fdd434b02ab46864fe5720c259a8a9bfb4912ab2
Isaac <irshields97@gmail.com>
2020-03-01 15:56:39 -0600

    Create uf01 test.

--- main/main.c
+++ main/main.c
@@ -109,21 +109,16 @@
 #define LIA_DATA_Z_LSB      (0X2C)
 #define LIA_DATA_Z_MSB      (0X2D)
 
+/* TEST TASK */
+#define TEST_CONFIG_SIZE        (2048)              /* size of memory allocated to the loop_task() task */
+#define TEST_CONFIG_PRIORITY    (10)                /* priority of the loop_task() task */
+#define TEST_PIN                (25)                /* use pin 33 */
+#define TEST_MASK               (1ULL<<TEST_PIN)    /* create bit mask for setup call */
+
 /*****************************************************************************/
 /* CONSTANTS */
 /*****************************************************************************/
 
-/* IMU CONFIG TASK */
-static const char * const 
-nvs_labels[NVS_LABEL_COUNT] =   {   
-                                    "ACC_OFFSET_X_L", "ACC_OFFSET_X_M",
-                                    "ACC_OFFSET_Y_L", "ACC_OFFSET_Y_M",
-                                    "ACC_OFFSET_Z_L", "ACC_OFFSET_Z_M",
-                                    "GYR_OFFSET_X_L", "GYR_OFFSET_X_M",
-                                    "GYR_OFFSET_Y_L", "GYR_OFFSET_Y_M",
-                                    "GYR_OFFSET_Z_L", "GYR_OFFSET_Z_M",
-                                    "ACC_RADIUS_L", "ACC_RADIUS_M"
-                                };
 /*****************************************************************************/
 /* INTERRUPT SERVICE ROUTINES */
 /*****************************************************************************/
@@ -131,60 +126,6 @@ nvs_labels[NVS_LABEL_COUNT] =   {
 /*****************************************************************************/
 /* HELPER FUNCTIONS */
 /*****************************************************************************/
-
-/*  
-    NAME:               delay
-    AURTHOR(S):         Isaac Shields
-    CALLED BY:          
-    PURPOSE:            Delays the program for x microseconds.
-    CALLING CONVENTION: Pass the number of microseconds you want to delay.  Do not use this for
-                        times greater than 10ms.  Instead, us vTaskDelay().  Don't call with 
-                        negative numbers.
-                        Example for 10 uS: delay(10);
-    CONDITIONS AT EXIT: This function has not return and does not modify any external variables.
-    DATE STARTED:       2/28/2020
-    UPDATE HISTORY:     See Git logs.
-    NOTES:              
-*/
-static void delay(int16_t uS)
-{
-    volatile uint8_t val; /* must be volatile to avoid compiler optimization */
-    while (uS > 0)
-    {
-        for(val = 0; val < 9; val++)
-        {
-        }
-        uS--;
-    }
-}
-
-/*  
-    NAME:               uart_init
-    AURTHOR(S):         Isaac Shields
-    CALLED BY:          app_main() in main.c
-    PURPOSE:            Initializes UART communication with BNO055.
-    CALLING CONVENTION: uart_init();
-    CONDITIONS AT EXIT: This function modifies the state of UART pins 1 and 3.  It also configures
-                        the UART0 peripheral.
-    DATE STARTED:       2/24/2020
-    UPDATE HISTORY:     See Git logs.
-    NOTES:              
-*/
-static void uart_init()
-{   
-    /* Configure UART for 115200 bps, 8N1, no parity. */
-    uart_config_t uart_config = 
-    {
-        .baud_rate = BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_NUM_0, &uart_config);
-    uart_set_pin(UART_NUM_0, IMU_TX, IMU_RX, RTS, CTS);
-    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
-}
 
 /*  
     NAME:               start_stop_init
@@ -209,112 +150,57 @@ static void start_stop_init()
     gpio_config(&start_stop);
 }
 
-/*  
-    NAME:               read_data
-    AURTHOR(S):         Isaac Shields
-    CALLED BY:          loop_task() in main.c
-    PURPOSE:            Reads raw linear acceleration data from the IMU.
-    CALLING CONVENTION: This function takes a pointer to a uint8_t type.  This pointer is filled with 
-                        IMU data.  The maximum size of data collected is 36KB.  If the functiion runs
-                        longer (uninterrupted by a user button press), it will abort.  TRUE means data was
-                        collected successfully and FALSE means data collection failed.
-                        is 36KB.
-    CONDITIONS AT EXIT: This function fills the buffer passed to it.
-    DATE STARTED:       2/27/2020
-    UPDATE HISTORY:     See Git logs.
-    NOTES:              
-*/
-//static bool read_data(uint8_t * data)
-//{
-    
-//}
-
 /*****************************************************************************/
 /* TASKS */
 /*****************************************************************************/
-
 /*  
-    NAME:               loop_task
+    NAME:               test_task
     AURTHOR(S):         Isaac Shields
     CALLED BY:          app_main() in main.c
-    PURPOSE:            Handles the main loop of the application.  It handles the three
-                        states of READY (waiting for button press), RECORDING (recording
-                        IMU data and waiting for button press), PROCESSING (processing IMU
-                        data), and SENDING (sending processed data to the phone).
-    CALLING CONVENTION: This must be called as a task, so use the convention below.
-                        xTaskCreate(loop_task, "loop", LOOP_SIZE, NULL, LOOP_PRIORITY, NULL);
-    CONDITIONS AT EXIT: This task will not exit.
-    DATE STARTED:       2/26/2020
+    PURPOSE:            Runs the unit test for this branch.  See teh git branch
+                        name for the test code.
+    CALLING CONVENTION: xTaskCreate(test_task, "test_task", IMU_CONFIG_SIZE, NULL, IMU_CONFIG_PRIORITY, NULL);
+    CONDITIONS AT EXIT: This function modifies the state of GPIO pin 33.
+    DATE STARTED:       2/24/2020
     UPDATE HISTORY:     See Git logs.
     NOTES:              
 */
-
-
-/*  
-    NAME:               imu_config_task
-    AURTHOR(S):         Isaac Shields
-    CALLED BY:          app_main() in main.c
-    PURPOSE:            Calibrates the IMU.  IMU callibration is only needed when data seems off.  We save
-                        the calibration in the flash and load it into the IMU after every reset.  This function
-                        generates new values for the calibration.  It is only entered if the user holds the
-                        START/STOP button during power on.
-    CALLING CONVENTION: xTaskCreate(imu_config_task, "imu_config", IMU_CONFIG_SIZE, NULL, IMU_CONFIG_PRIORITY, NULL);
-    CONDITIONS AT EXIT: This function does not exit.
-    DATE STARTED:       2/28/2020
-    UPDATE HISTORY:     See Git logs.
-    NOTES:              NVS code used from nvs.value.example.main.c from Espessif.
-*/
-static void imu_config_task()
+static void test_task()
 {
-    uint8_t temp;   // remove when done testing
+    /* Configure output port for testing */
+    gpio_config_t test;
+    test.intr_type = GPIO_PIN_INTR_DISABLE;
+    test.mode = GPIO_MODE_OUTPUT;
+    test.pin_bit_mask = TEST_MASK;
+    test.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    test.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&test);
     
-    while (1)
+    if (gpio_get_level(START_STOP_PIN) == 1) /* if button is not pressed */
     {
-    if (gpio_get_level(GPIO_NUM_33) == 0) /* if button is pressed */
+        while(1)
+        {
+            if (gpio_get_level(START_STOP_PIN) == 0)
+            {
+                vTaskDelay( 2000 / portTICK_PERIOD_MS ); /* two second debounce */
+                gpio_set_level(TEST_PIN, 0);
+                vTaskDelay( 100 / portTICK_PERIOD_MS );
+                gpio_set_level(TEST_PIN, 1);
+                vTaskDelay( 100 / portTICK_PERIOD_MS );
+            }
+            vTaskDelay( 100 / portTICK_PERIOD_MS );
+        } 
+    }
+    else /* button is pressed */
     {
-        vTaskDelay(5000 / portTICK_PERIOD_MS); // debounce
-        
-        /**************************************/
-        /* INITIALIZE NVS */
-        /**************************************/
-        esp_err_t err = nvs_flash_init();
-        if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) 
+        while(1)
         {
-            /* NVS partition was truncated and needs to be erased */
-            ESP_ERROR_CHECK(nvs_flash_erase());
-            err = nvs_flash_init();
-        }
-        ESP_ERROR_CHECK( err );
-        
-        /**************************************/
-        /* OPEN NVS HANDLES */
-        /**************************************/
-        nvs_handle_t handles[NVS_LABEL_COUNT];   /* create nvs handle for each piece of calibration data */
-        for (int i = 0; i < NVS_LABEL_COUNT; i++)
-        {
-            err = nvs_open(nvs_labels[i], NVS_READWRITE, &handles[i]);/* open nvs */ // add error handling
-            if (err != ESP_OK)
-                printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
-            err = nvs_set_u8(handles[i], nvs_labels[i], (uint8_t) i); /* write nvs */   // do error checking
-            if (err != ESP_OK)
-                printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
-            err = nvs_commit(handles[i]);   // do error checking
-            if (err != ESP_OK)
-                printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
-            err = nvs_get_u8(handles[i], nvs_labels[i], &temp);
-            if (err != ESP_OK)
-                printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
-            uart_write_bytes(UART_NUM_0, (char *) &temp, 1);
-                
+            gpio_set_level(TEST_PIN, 0);
+            vTaskDelay( 100 / portTICK_PERIOD_MS );
+            gpio_set_level(TEST_PIN, 1);
+            vTaskDelay( 100 / portTICK_PERIOD_MS );
         }
     }
-    else    /* if button is not pressed */
-    {
-        /* delay to feed the watchdog */
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-    }
-    }
-    
 }
 
 /*  
@@ -331,8 +217,6 @@ static void imu_config_task()
 */
 void app_main()
 {
-    uart_init();
     start_stop_init();
-    //xTaskCreate(loop_task, "loop", LOOP_SIZE, NULL, LOOP_PRIORITY, NULL);
-    xTaskCreate(imu_config_task, "imu_config", IMU_CONFIG_SIZE, NULL, IMU_CONFIG_PRIORITY, NULL);
+    xTaskCreate(test_task, "test_task", TEST_CONFIG_SIZE, NULL, TEST_CONFIG_PRIORITY, NULL);
 }
